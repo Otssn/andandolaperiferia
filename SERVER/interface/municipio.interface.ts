cc9fcc6c7a2f58f067bd8f43939b2e313168d802
analistaTASS <analistadesarrollo5@tass.com.co>
2023-04-05 18:51:17 -0500

    Se crea interface para responder a las peticiones

--- SERVER/interface/municipio.interface.ts
+++ SERVER/interface/municipio.interface.ts
@@ -10,3 +10,10 @@ export interface IResponseData {
     data: any,
     error: boolean
 }
+
+export interface IResponseRequest {
+    completada: boolean,
+    error: boolean,
+    mensaje: string,
+    data: any
+}
