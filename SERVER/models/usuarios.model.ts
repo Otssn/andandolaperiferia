01890f1835ee0fa14ee7826ba69a3f6bed052abd
Otssn <pabon.esteban13@gmail.com>
2023-04-13 10:01:40 -0500

    Creacion del modelo usuario

---
+++ SERVER/models/usuarios.model.ts
@@ -0,0 +1,26 @@
+import { model, Schema } from 'mongoose';
+import mongoose from 'mongoose';
+import { DateTime } from 'luxon';
+
+const usuarioSchema = new Schema({
+    nombre:{
+        type: String,
+        require: true
+    },
+    correo:{
+        type: String,
+        require: true
+    },
+    usuario:{
+        type: String,
+        require: true
+    },
+    contraseña:{
+        type: String,
+        require: true
+    },
+    idPerfil:{
+        type: Number,
+        require: true
+    }
+});
